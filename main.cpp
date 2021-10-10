2cc4707e7f162010af2d1b9f611be11f743438bd
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-11 02:15:13 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+  class MyClass {       
+  // The class
+  public:             
+  // Access specifier
+    int myNum;        // 
+  Attribute (int variable)
+    string myString;  // 
+  Attribute (string variable)
+};
+
+  
